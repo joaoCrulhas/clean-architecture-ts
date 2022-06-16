5e70284327fc20a134d74052b226af50bafd5b62
joaocrulhas <joao@isarlend.com>
2022-06-16 10:56:03 +0200

    first commit

--- src/presentation/controllers/signup-controller/index.ts
+++ src/presentation/controllers/signup-controller/index.ts
@@ -1,12 +1,12 @@
 export default class SignupController {
   handle(httpRequest: any): any {
-    if(!httpRequest.body.name) {
+    if (!httpRequest.body.name) {
       return {
         statusCode: 400,
         body: new Error("Missing param: name"),
       };
     }
-    if(!httpRequest.body.password) {
+    if (!httpRequest.body.password) {
       return {
         statusCode: 400,
         body: new Error("Missing param: password"),
