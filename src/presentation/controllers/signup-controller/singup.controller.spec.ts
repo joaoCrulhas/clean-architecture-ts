c2cd961c7f53fbdfed7171a82a04cbd15e3727d4
joaocrulhas <joao@isarlend.com>
2022-06-16 11:41:45 +0200

    first commit

--- src/presentation/controllers/signup-controller/singup.controller.spec.ts
+++ src/presentation/controllers/signup-controller/singup.controller.spec.ts
@@ -1,8 +1,12 @@
 import SignupController from "./index";
 
+const makeSut = () => {
+  return new SignupController();
+}
+
 describe("Signup Controller", () => {
   test("Should return 400 if no username  is not provided", () => {
-    const sut = new SignupController();
+    const sut = makeSut();
     const httpRequest = {
       body: {
         email: "das@gmail.com",
@@ -16,7 +20,7 @@ describe("Signup Controller", () => {
     expect(response.error).toEqual("Missing param: username");
   });
   test("Should return 400 if no password  is not provided", () => {
-    const sut = new SignupController();
+    const sut = makeSut();
     const httpRequest = {
       body: {
         email: "das@gmail.com",
@@ -30,7 +34,7 @@ describe("Signup Controller", () => {
     expect(response.error).toEqual("Missing param: password");
   });
   test("Should return 400 if no password_confirmation is not provided", () => {
-    const sut = new SignupController();
+    const sut = makeSut();
     const httpRequest = {
       body: {
         email: "das@gmail.com",
@@ -44,11 +48,27 @@ describe("Signup Controller", () => {
     expect(response.error).toEqual("Missing param: password_confirmation");
   });
   test("Should return 400 if no email is not provided", () => {
-    const sut = new SignupController();
+    const sut = makeSut();
     const httpRequest = {
       body: {
         username: "name",
-        password: "das@gmail.com",
+        password: "password",
+        password_confirmation: "password",
+      },
+    };
+    const response = sut.handle(httpRequest);
+    expect(response.isFailure).toBe(true);
+    expect(response.isSuccess).toBe(false);
+    expect(response.error).toEqual("Missing param: email");
+  });
+
+  test("Should return 400 if an invalid email is provided", () => {
+    const sut = makeSut();
+    const httpRequest = {
+      body: {
+        email: "dsadsad",
+        username: "name",
+        password: "password",
         password_confirmation: "password",
       },
     };
