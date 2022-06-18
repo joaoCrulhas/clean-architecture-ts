d163a31a8e608bfbfe740f6850dc323207ff8276
joaocrulhas <joao@isarlend.com>
2022-06-18 08:31:42 +0200

    first commit

---
+++ src/presentation/lib/email-validator/email-validator-adapter.spec.ts
@@ -0,0 +1,23 @@
+import {EmailValidator} from "../../protocols/email-validator.protocol";
+
+class EmailValidatorAdapter implements EmailValidator {
+    isValid(email: string): boolean {
+        const re = /\S+@\S+\.\S+/;
+        return re.test(email);
+    }
+
+}
+describe("EmailValidator adapter", () => {
+    test("should return false if validator returns false", () => {
+        const sut = new EmailValidatorAdapter();
+        const response = sut.isValid('valid_mail@');
+        expect(response).toBe(false);
+        console.log(response);
+    });
+    test("should return false if validator returns false", () => {
+        const sut = new EmailValidatorAdapter();
+        const response = sut.isValid('valid_mail@gmail.com');
+        expect(response).toBe(true);
+    });
+
+})
