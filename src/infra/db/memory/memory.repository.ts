caa77ce857bacfa0410af7c5ca7519b43c141c5f
joaocrulhas <joao@isarlend.com>
2022-06-19 13:43:17 +0200

    first commit

--- src/infra/db/memory/memory.repository.ts
+++ src/infra/db/memory/memory.repository.ts
@@ -1,27 +1,25 @@
-import {AddAccountRepository} from "../../../datalayer/usecases/add-account/protocols/add-account-repository.protocol";
-import {AddAccountRequest} from "../../../domain/add-account/add-account-request.model";
-import {AccountModel} from "../../../domain/add-account/add-account.protocol";
+import { AddAccountRepository } from "../../../datalayer/usecases/add-account/protocols/add-account-repository.protocol";
+import { AddAccountRequest } from "../../../domain/add-account/add-account-request.model";
+import { AccountModel } from "../../../domain/add-account/add-account.protocol";
 
 class MemoryRepository implements AddAccountRepository {
-    constructor(private  readonly db: Array<AccountModel>){}
-    add(account: AddAccountRequest): Promise<AccountModel> {
-        return new Promise((resolve, reject) => {
-            const id = `user_${this.db.length + 1}`;
-            this.db.push({
-                email: account.email,
-                id,
-                username: account.username,
-                timestamp: new Date()
-            })
-            resolve({
-                email: account.email,
-                id,
-                username: account.username,
-                timestamp: new Date()
-            })
-        });
-    }
+  constructor(private readonly db: Array<AccountModel>) {}
+  add(account: AddAccountRequest): Promise<AccountModel> {
+    return new Promise((resolve, reject) => {
+      const id = `user_${this.db.length + 1}`;
+      this.db.push({
+        email: account.email,
+        id,
+        username: account.username,
+        timestamp: new Date(),
+      });
+      resolve({
+        email: account.email,
+        id,
+        username: account.username,
+        timestamp: new Date(),
+      });
+    });
+  }
 }
-export {
-    MemoryRepository
-}
+export { MemoryRepository };
