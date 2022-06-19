50829f637c9d0d301af32c1af3b501def0f85f66
joaocrulhas <joao@isarlend.com>
2022-06-19 08:13:56 +0200

    first commit

--- src/lib/encrypter/bcrypt-adapter.spec.ts
+++ src/lib/encrypter/bcrypt-adapter.spec.ts
@@ -1,5 +1,10 @@
 import { BcryptAdapter } from "./brcypt.adapter";
 import bcrypt from "bcrypt";
+jest.mock('bcrypt', () => ({
+  async hash(): Promise<string> {
+    return Promise.resolve("hash");
+  }
+}))
 describe("BcryptAdapter", function () {
   test("should call bcrypt with correct parameters", async function () {
     const sut = new BcryptAdapter(12);
@@ -7,5 +12,11 @@ describe("BcryptAdapter", function () {
     await sut.encrypt("password");
     expect(spyBcrypt).toHaveBeenCalled();
     expect(spyBcrypt).toHaveBeenCalledWith("password", 12);
+  });
+
+  test("should return a value if bcrypt is executed", async function () {
+    const sut = new BcryptAdapter(12);
+    const response = await sut.encrypt("password");
+    expect(response.getValue()).toEqual("hash");
   });
 });
